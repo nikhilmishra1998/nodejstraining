2cbf90b9c725c83e2a0059d45e73b646eabd6395
nikhilmishra1998 <mishra123nikhil@gmail.com>
2020-06-22 19:41:08 +0530

    Added model along with its logic

--- ejs/controllers/productController.js
+++ ejs/controllers/productController.js
@@ -1,13 +1,12 @@
-
-const products = [];
+const Products = require('./../model/product');
 
 // Here we will show the add product page to the client.
 exports.getAddProduct = (req, res, next) => {
     res.render(
         'add-product', 
         {
-            pageTitle : 'Add Products', 
-            page: 'Add Product'
+            pageTitle   : 'Add Products', 
+            page        : 'Add Product'
         }
     );
 };
@@ -15,13 +14,14 @@ exports.getAddProduct = (req, res, next) => {
 
 // Here we will add product in server side to use data further.
 exports.postAddProduct = (req, res, next) => {
-    products.push({title:req.body.title});
+    const product = new Products(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 // Here we will show the product to our user 
 exports.getListProduct = (req, res, next) => {
-    
+    const products = Products.fetchAll();
     res.render(
         'shop', 
         {
